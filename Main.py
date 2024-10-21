ba5831163c18ac078b61118b2b4c505bad616b7a
Janice Ivana <jiva0001@student.monash.edu>
2024-10-21 12:53:49 +1100

    Update streamlit

---
+++ Main.py
@@ -0,0 +1,13 @@
+#######################
+# Import libraries
+import streamlit as st
+
+
+st.header("Welcome to Janice Data Science Project Webpage!")
+
+st.subheader("Introduction")
+
+st.subheader("Meet our team members!")
+st.markdown("Janice Ivana")
+st.image("jan.jpg")
+st.write("Hi there! I'm a fresh data science graduate from Monash University, this webpage will be my main portofolio to show my work I have done so far :)")
