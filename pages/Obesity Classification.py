5b16650e1a6a99989dbf761c9a7b7d1d35d133ed
Janice Ivana <jiva0001@student.monash.edu>
2024-10-27 15:00:04 +1100

    Update Obesity Classification.py

--- pages/Obesity Classification.py
+++ pages/Obesity Classification.py
@@ -40,7 +40,7 @@ st.write(levels.info())
 st.subheader("Conclusion")
 st.markdown("The first dataset shows that the data is clean based on the non-existence null entries on each column. This dataset was taken from UCI library. It has been cleaned using techniques: z-score normalization, one-hot encoding, outlier removal, min-max scaling, and feature selection.")
 st.markdown("The target column 'NObeyesdad' contains the following encoding of Obesity Levels:")
-st.write(""" 
+st.markdown(""" 
             Insufficient_Weight: 0
          
             Normal_Weight:1
